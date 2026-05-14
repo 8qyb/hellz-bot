a00ad3406121e04344ed3ec2faf6959848b8d6b5
8qyb <fake8qyb@gmail.com>
2026-05-14 15:40:12 +0200

    fix syntax error and full code update

--- commands/fun/gstart.js
+++ commands/fun/gstart.js
@@ -8,7 +8,7 @@ module.exports = {
         .addStringOption(opt => opt.setName('duration').setDescription('Ex: 1h, 1d, 10m').setRequired(true))
         .addIntegerOption(opt => opt.setName('winners').setDescription('Number of winners').setRequired(true))
         .addStringOption(opt => opt.setName('prize').setDescription('What are you giving away?').setRequired(true))
-        .addBooleanOption(opt => opt.setName('dm_winner').setDescription('Should the winner be DM'd?'))
+        .addBooleanOption(opt => opt.setName('dm_winner').setDescription('Should the winner be DM\'d?')) // Fixed line
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
     async execute(interaction) {
         const duration = interaction.options.getString('duration');
@@ -16,8 +16,11 @@ module.exports = {
         const prize = interaction.options.getString('prize');
         const dmWinner = interaction.options.getBoolean('dm_winner') ?? false;
 
+        const giveawayDuration = ms(duration);
+        if (!giveawayDuration) return interaction.reply({ content: 'Invalid duration!', ephemeral: true });
+
         await interaction.client.giveawaysManager.start(interaction.channel, {
-            duration: ms(duration),
+            duration: giveawayDuration,
             winnerCount,
             prize,
             messages: {
@@ -27,13 +30,10 @@ module.exports = {
                 winMessage: dmWinner 
                     ? 'Congratulations, {winners}! You won **{prize}**! Check your DMs.' 
                     : 'Congratulations, {winners}! You won **{prize}**!'
-            }
-        }).then((data) => {
-            if (dmWinner) {
-                // We'll handle the DM in the giveawayEnd event below
-                data.extraData = { dmEnabled: true }; 
-            }
-            interaction.reply({ content: 'Giveaway started!', ephemeral: true });
+            },
+            extraData: { dmEnabled: dmWinner }
         });
+
+        await interaction.reply({ content: 'Giveaway started!', ephemeral: true });
     }
 };
